b69bf8c36000b203b9668fccc80bae3466482346
Adam Novak <anovak@soe.ucsc.edu>
2016-05-25 16:46:49 -0700

    Try a different approach to graph augmenting

--- src/augment.hpp
+++ src/augment.hpp
@@ -18,11 +18,11 @@ namespace vg {
 
 using namespace std;
 
-// We need to break apart nodes but remember where they came from to update
-// edges. Wrap all this up in this class.  For a position in the input graph, we
-// can have one or more nodes in the augmented graph (ref, alts), so we map to
-// colelctions of nodes. We also annotate all the nodes and edges with how much
-// support they have.
+// An AugmentedGraph wraps a VG graph that is partly embedded in another vg
+// graph, with only deletions, pure insertions, and SNPs being added on top.
+// Given a node and offset in the original graph, we can find where that lands
+// in the new graph. We also track support for nodes and edges in the augmented
+// graph, in terms of forward and reverse strand reads.
 struct AugmentedGraph {
     // Represents support on the forward and reverse strands. One gets
     // associated with every node base and every edge.
@@ -33,108 +33,74 @@ struct AugmentedGraph {
         int reverse = 0;
     };
 
-    // This represents all the nodes created for a run of reference positions: one ref node and all the alt nodes.
-    struct Entry {
-        Node* ref;
-        vector<Support> ref_support;
-        vector<Node*> alts;
-        vector<vector<Support>> alt_support;
-        
-        // Make a new Entry for just a ref node
-        Entry(Node* ref): ref(ref), ref_support(ref->sequence().size()) {
-        }
-        
-        // Make a new entry with zero support
-        Entry(Node* ref, vector<Node*> alts): ref(ref), ref_support(ref->sequence().size()),
-            alts(alts), alt_support(alts.size()) {
-            
-            for(size_t i = 0; i < alts.size(); i++) {
-                // Allocate support for each base of each alt.
-                alt_support[i] = vector<Support>(alts[i]->sequence().size());
-            }
-        }
-        
-        // Add a new alt with no support. Must not be in here already.
-        void push_back(Node* alt) {
-            alts.push_back(alt);
-            alt_support.push_back(vector<Support>(alt->sequence().size()));
-        }
-        
-        // Get the number of nodes here (ref plus alts)
-        int size() {
-            return 1 + alts.size();
-        }
-        
-        // Get the ith ref or alt node. i must be <= size().
-        Node*& operator[](int i) {
-            if(i == 0) {
-                return ref;
-            } else {
-                return alts.at(i - 1);
-            }
-        }
-        
-        // Get the support for the ith ref or alt node. i must be <= size().
-        vector<Support>& sup(int i) {
-            if(i == 0) {
-                return ref_support;
-            } else {
-                return alt_support.at(i - 1);
-            }
-        }
-    };
-    // Start offset in original graph node -> ref and all alts in augmented graph
-    typedef map<int, Entry> NodeMap;
-    // Node id in original graph to NodeMap saying what alts are available for each part.
-    typedef hash_map<int64_t, NodeMap> NodeHash;
-    
-    // This stores the embedding of the augmented graph in the original graph,
-    // or equivalently the fluffing-up of the original graph into the augmented
-    // graph.
-    NodeHash index;
-    
-    // This holds support for all the edges in the augmented graph. When edges
-    // are created or destroyed, we need to update this.
-    map<Edge*, Support> edge_support;
-
-    // We keep around the augmented graph that holds all the ref and alt nodes
-    // we have been pointing to in the index, and all the edges we have support
-    // annotations for. Instead of using its divide_node-type methods, we handle
-    // all the node and edge rewriting ourselves in order to keep our
-    // annotations in sync.
+    // This holds the actual augmented graph, which starts out as a copy of the
+    // original graph.
     VG graph;
     
-    // We handle our own ID generation because we would like new node IDs to all
-    // come after old node IDs.
-    id_t next_id;
+    // Map from original graph ID and then offset along the original node to the
+    // new split-up node in the augmented graph.
+    map<id_t, map<int, Node*>> index;
+    
+    // We want some way to know if edges are reference (and we should give
+    // support to them when we give support to the bases before them) or not.
+    // This is to handle the ref edge that would be opposite an insert on the
+    // last base of a node with multiple edges out of its end. For this we just
+    // refer back to the original graph, which can tell us what edges are on a
+    // node, and we can find the corresponding edges in our graph and follow
+    // them. We also use this to know where to connect a SNP that lands at the
+    // start or end of a node.
+    const VG& original;
 
-    // Break an original-graph node (if needed) so that the specified side of
-    // the specified base becomes free, so we can attach edges to it. Fixes up
-    // the index and preserves all the support counts. Returns the reference
-    // Node* on the left of the break if left_side is true, and the one on the
-    // right of the break otherwise.
-    Node* break_end(const Node* orig_node, int offset, bool left_side);    
+    // Make an AugmentedGraph from an original VG graph, which gets copied to
+    // create the augmented version.
+    AugmentedGraph(const VG& original);
     
-    // Add an alt node (or add support to the ref node) starting at the given
-    // offset in the given original node. Takes care of breaking the reference
-    // node, creating (and deduplicating) the alt node, and wiring everything
-    // up. Optionally takes some support to either give to that alt node or add
-    // to the existing alt node (and its ref-touching edges).
-    void add_fragment(const Node* orig_node, int offset, int ref_length, string alt_sequence, Support support = Support());
+    // Divide a node from the original graph so as to free the given side (left
+    // or right) for edge connection. Calling this invalidates any stored
+    // support values.
+    void divide_node(id_t original_id, int pos, bool is_left);
     
-    // Add the given support to an original edge. Adds support only to the ref-
-    // ref edge, not any edges created connecting alts to each other.
-    void add_edge(const Edge* orig_edge, Support support);
+    // Get the NodeSide in the augmented graph that the given side of the given
+    // original base now belongs to, or raise an exception if no cut has been
+    // made to produce such a side.
+    NodeSide find_side(id_t original_id, int pos, bool is_left);
     
-    // Create (and/or add support to) an edge between positions on original nodes.
-    void add_edge(const Node* orig_from, int from_offset, bool from_start,
-        const Node* orig_to, int to_offset, bool to_end, Support support = Support());
+    // Get the list of NodeSides in the augmented graph that correspond to the
+    // ends of nodes that were attached to the given side of the given base in
+    // the original graph. Raises an exception if that side of that base was not
+    // at the edge of a node in the original graph. Used to determine which
+    // other nodes to go to after an insert at the end of a node, or
+    // after/before a SNP at the end/start of a node.
+    vector<NodeSide> find_originally_attached_sides(id_t original_id, int pos, bool is_left);
+    
+    // Add support for a novel, deletion edge. Adds the edge if it doesn't exist.
+    void add_support_for_deletion_edge(id_t from, int from_offset, bool from_start, id_t to, int to_offset, bool to_end, Support support);
+    
+    
+    // Add support for the given base of the given original node, and for all
+    // reference edges that come after it (if any). It's a bit wonky because it
+    // duplicates support multiple ways going e.g. into a SNP, but since when we
+    // do the actual calling we only care about support for edges opposite
+    // inserts and edges that are deletions, it should work out. Needs to be
+    // called for anything that isn't an insert.
+    void add_support_for_base_and_subsequent_edges(id_t original_id, int pos, Support support);
+    
+    // Call this instead of the above for inserts (TODO: and deletes? Do they delete the base they land on or not?)
+    void add_support_for_base_and_subsequent_edges(id_t original_id, int pos, Support support);
+    
+    // Add an insert. Can't call multiple times, because that will make
+    // multiple inserts. You need to aggregate support beforehand.
+    void add_insert_with_support(id_t original_id, int pos, string inserted, Support support);
+    
+    
+    // Like above, but for a SNP
+    void add_snp_with_support(id_t original_id, int pos, string snp, Support support);
+    
+    // TODO: function to take a pileup, make cuts, scan it again, aggregate SNPs
+    // and inserts, and then throw in all the new stuff with its support/support
+    // for old stuff.
     
 };
-
-// We can serialize these things for debugging.
-ostream& operator<<(ostream& os, const AugmentedGraph::NodeMap& nm);
-ostream& operator<<(ostream& os, AugmentedGraph::Entry entry);
 
 }
 
