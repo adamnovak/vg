5887299ac0744b09d4cbf98cd26ded2dfe872feb
Adam Novak <anovak@soe.ucsc.edu>
2025-06-17 09:29:05 -0700

    Add vg chunk option names to match help

--- src/subcommand/chunk_main.cpp
+++ src/subcommand/chunk_main.cpp
@@ -147,8 +147,10 @@ int main_chunk(int argc, char** argv) {
             {"output-bed", required_argument, 0, 'E'},
             {"prefix", required_argument, 0, 'b'},
             {"context", required_argument, 0, 'c'},
-            {"id-ranges", no_argument, 0, 'r'},
-            {"id-range", no_argument, 0, 'R'},
+            {"node-ranges", no_argument, 0, 'r'},
+            {"id-ranges", no_argument, 0, 'r'}, // Old, deprecated name
+            {"node-range", no_argument, 0, 'R'},
+            {"id-range", no_argument, 0, 'R'}, // Old, deprecated name
             {"trace", no_argument, 0, 'T'},
             {"no-embedded-haplotypes", no_argument, 0, OPT_NO_EMBEDDED_HAPLOTYPES},
             {"fully-contained", no_argument, 0, 'f'},
